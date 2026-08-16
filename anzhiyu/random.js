d854098f3d5a1d6d2fd57754cfb22a8514d226df
zzh031208 <2896780219@qq.com>
2026-08-16 21:08:29 +0800

    deploy: rebuild site with Git tutorial post

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/08/16/hello-world/"];function toRandomPost(){
+var posts=["2026/08/16/Git使用教程/","2026/08/16/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
