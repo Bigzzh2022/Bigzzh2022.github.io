e359f6409d90733ee152ec3b54bb4dc2e9ac765e
zzh031208 <2896780219@qq.com>
2026-08-19 21:17:57 +0800

    fix: remove public/ from tracking and add it to .gitignore

--- public/anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2026/08/16/Git使用教程/","2026/08/19/博客使用教程/","2026/08/16/hello-world/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
